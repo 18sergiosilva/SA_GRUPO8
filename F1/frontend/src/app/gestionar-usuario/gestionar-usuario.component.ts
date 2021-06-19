3b726751ae442c92c0d7bbf74f15338392d5d870
Francisco Lezana <fraaanciscolezana8@gmail.com>
2021-06-19 01:07:43 -0600

    add: gestion de usuarios

--- F1/frontend/src/app/gestionar-usuario/gestionar-usuario.component.ts
+++ F1/frontend/src/app/gestionar-usuario/gestionar-usuario.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import Swal from 'sweetalert2';
+import { Utils } from '../utils/utils';
 
 @Component({
   selector: 'app-gestionar-usuario',
@@ -7,9 +11,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GestionarUsuarioComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private http: HttpClient) { }
+
+  // Declaraciones
+  users = [];
+  username = "";
+  tabletitle="LISTA DE USUARIOS"
+
+  endpoint = "http://18.118.255.26:3005";
 
   ngOnInit() {
+    if(localStorage.getItem('logged') === '0'){
+      this.router.navigate(['login']);
+    }
+    this.cargarUsers();
+  }
+
+  cargarUsers(): boolean {
+    this.http.get(this.endpoint+'/users/getAll')
+      .toPromise().then((data: any) => {
+        console.log(data.data);
+        this.users = data.data;
+      });
+    return true;
+  }
+
+  cambiarStatus(u_username: string):boolean {
+    this.http.post(this.endpoint+'/users/deleteUser',
+    {
+      'username': u_username
+    }).toPromise().then((data: any) => {
+      Swal.fire({
+        text: 'Usuario Eliminado',
+        icon: 'success',
+        confirmButtonText: 'Aceptar',
+      })
+      this.cargarUsers(); 
+      return true;
+    });
+    return false;
+
   }
 
 }
