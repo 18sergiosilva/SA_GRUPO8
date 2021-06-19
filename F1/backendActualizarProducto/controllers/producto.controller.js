411578dffd6581b81102e0afbd32c08904f47def
Luis Robles <lrobles2408@gmail.com>
2021-06-19 16:22:07 -0600

    hotfix finalx3!

--- F1/backendActualizarProducto/controllers/producto.controller.js
+++ F1/backendActualizarProducto/controllers/producto.controller.js
@@ -5,7 +5,7 @@ var logs = require('../utils/log');
 module.exports = {
     actualizar: (req, res) => {
         const idProducto = req.params.id;
-        const { nombre, precio, descripcion, imagen, Editorial, Genero, Stock} = req.body;
+        const { nombre, precio, descripcion, Editorial, Genero, Stock} = req.body;
         var update = {};
 
         //console.log("id --> "+ idProducto);
@@ -76,49 +76,15 @@ module.exports = {
                 //console.log(producto);
                 productoActual = producto;
                 //console.log("variable -->" +productoActual);
-
-                if (imagen) {
-                    console.log("modificando imagen --->");
-                    borrarImagen(producto.urlImagen).then(data => {
-                        subirImagen(imagen, producto.sku).then(imagen => {
-                            //console.log(imagen);
-                            update.imagen = imagen.Location;
-                            update.urlImagen = imagen.Key;
-                            
-                            productoActualizado.imagen = imagen.Location;
-                            productoActualizado.urlImagen = imagen.Key;
-
-                            modelProducto.findByIdAndUpdate(idProducto, update).then(producto => {
-                                logs.logActualizarProducto("Actualizar producto", usuario,idUsuario,"Admin","servicio actualizar producto", productoActualizado, productoActual);
-                                return res.send({
-                                    mensaje: "Producto actualizado exitosamente"
-                                });
-                            })
-                        }).catch(err => {
-                            logs.logErrores("Error:: Ocurrio un error al subir la nueva imagen", usuario,idUsuario,"Sistema","servicio actualizar Producto",productoActualizado);
-                            console.log(err);
-                            return res.status(500).send({
-                                mensaje: "Ocurrio un error al subir la nueva imagen"
-                            })
-                        })
-                    })
-                        .catch(err => {
-                            logs.logErrores("Error:: Ocurrio un error al eliminar la imagen anterior", usuario,idUsuario,"Sistema","servicio actualizar Producto",productoActualizado);
-                            console.log(err);
-                            return res.status(500).send({
-                                mensaje: "Ocurrio un error al eliminar la imagen anterior"
-                            })
-                        });
-                } else {
-                    modelProducto.findByIdAndUpdate(idProducto, update).then(producto => {
+                modelProducto.findByIdAndUpdate(idProducto, update).then(producto => {
                         
-                        logs.logActualizarProducto("Actualizar producto", usuario,idUsuario,"Admin","servicio actualizar producto", productoActualizado, productoActual);
-                        
-                        return res.send({
-                            mensaje: "Producto actualizado exitosamente"
-                        });
-                    })
-                }
+                    logs.logActualizarProducto("Actualizar producto", usuario,idUsuario,"Admin","servicio actualizar producto", productoActualizado, productoActual);
+                    
+                    return res.send({
+                        mensaje: "Producto actualizado exitosamente"
+                    });
+                })
+                
             } else {
                 logs.logErrores("Error:: No existe producto con id " + idProducto, usuario,idUsuario,"Admin","servicio actualizar Producto",productoActualizado);
                 return res.status(404).send({
